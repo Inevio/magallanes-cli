dd8caab2ac38c5a0d794ae0d833548eac6916b3e
Javier Garmon <jgarmon@inevio.com>
2016-05-16 17:41:18 +0200

    Implemented addImageToNodeByName

--- cmd/addImageToNodeByName.js
+++ cmd/addImageToNodeByName.js
@@ -1,3 +1,11 @@
 'use strict';
 
-module.exports = function(){};
+module.exports = function( client, argv ){
+
+  client.request( 'addImageToNodeByName', argv[ 2 ], argv[ 4 ], function( error, res ){
+    console.log( arguments );
+    console.log('');
+    process.exit();
+  });
+
+};
